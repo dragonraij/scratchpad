cb34d30409f184b5c8ad1852d497569696de7ba8
Raj Prasad <rajeshwarrprasad@yahoo.com>
2015-08-17 19:14:40 +1200

    Working app
    Finished rough working version of weather app

--- MSA/Weather2/Weather2/Program.cs
+++ MSA/Weather2/Weather2/Program.cs
@@ -1,6 +1,8 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -10,6 +12,36 @@ namespace Weather2
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Weather app - enter City:");
+
+            string city = Console.ReadLine();
+
+            getWeather(city);
+
+            // Prevent app from closing
+            Console.ReadKey();
+        }
+
+            static async void getWeather(string city) {
+            Console.WriteLine("Loading weather data ... ");
+
+            try
+            {
+                HttpClient client = new HttpClient();
+
+                Weather_Object.RootObject rootObject = JsonConvert.DeserializeObject<Weather_Object.RootObject>(await client.GetStringAsync(new Uri("http://api.openweathermap.org/data/2.5/weather?q=" + city + "&units=metric&APPID=440e3d0ee33a977c5e2fff6bc12448ee")));
+
+                Console.WriteLine("Current City is : "+rootObject.name);
+                Console.WriteLine("Temperate : \t"+ rootObject.main.temp+"C");
+                Console.WriteLine("Wind Speed : \t" + rootObject.wind.speed);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+
+            }
+            Console.WriteLine("Press any key to exit");
+
         }
     }
 }
