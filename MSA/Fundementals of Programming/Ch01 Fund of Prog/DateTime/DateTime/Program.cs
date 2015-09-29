1c55da059bfe41d49f7a41d2463e4b990b7da93c
Raj Prasad <rajeshwarrprasad@yahoo.com>
2015-09-29 19:27:25 +1300

    Print Data and time

---
+++ MSA/Fundementals of Programming/Ch01 Fund of Prog/DateTime/DateTime/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Date_Time
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("{0} : {1}", "The current time is", DateTime.Now.ToShortTimeString());
+            Console.WriteLine("{0} : {1}", "Todays date is", DateTime.Now.ToLongDateString());
+            Console.ReadKey();
+        }
+    }
+}
