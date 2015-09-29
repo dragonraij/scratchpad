0f29e880e2dd66616709d874c73794013a3add10
Raj Prasad <rajeshwarrprasad@yahoo.com>
2015-09-29 19:45:39 +1300

    Print Series of First 100 numbers

---
+++ MSA/Fundementals of Programming/Ch01 Fund of Prog/FirstHundred/FirstHundred/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FirstHundred
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Prints the first 100 numbers of the sequence: 2, -3, 4, -5, 6 ...");
+
+            int num = 1;
+
+            for (int i = 0; i < 100; i++)
+            {
+                num++;
+                if (num%2==0)
+                {
+                    Console.WriteLine(-num);
+                }
+                else
+                {
+                    Console.WriteLine(num);
+                }
+            }
+            Console.ReadKey();
+        }
+    }
+}
