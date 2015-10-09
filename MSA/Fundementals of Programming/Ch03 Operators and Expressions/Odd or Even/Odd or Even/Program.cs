c45675c9a795c61b06994fca66745b1b33b36471
Raj Prasad <rajeshwarrprasad@yahoo.com>
2015-10-09 20:19:21 +1300

    check if even

---
+++ MSA/Fundementals of Programming/Ch03 Operators and Expressions/Odd or Even/Odd or Even/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Odd_or_Even
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //declare varialbes
+            int num = 30;
+
+
+            if (num%2==0)
+            {
+                Console.WriteLine("it is even");
+            }
+            else
+            {
+                Console.WriteLine("it is odd isnt it?");
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
