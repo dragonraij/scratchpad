49b82e46d2ee5cd7ab01cf4765e98daf22408733
Raj Prasad <rajeshwarrprasad@yahoo.com>
2015-10-16 20:06:18 +1300

    Switch case with multiple values

--- MSA/Fundementals of Programming/Ch05 Conditionals/Conditions1/Conditions1/Program.cs
+++ MSA/Fundementals of Programming/Ch05 Conditionals/Conditions1/Conditions1/Program.cs
@@ -173,7 +173,41 @@ namespace Conditions1
 
             }
 
+
+
+            //10. Write a program that applies bonus points to given scores in the range [1…9] by the following rules:
+            //- If the score is between 1 and 3, the program multiplies it by 10.
+            //- If the score is between 4 and 6, the program multiplies it by 100.
+            //- If the score is between 7 and 9, the program multiplies it by 1000.
+            //- If the score is 0 or more than 9, the program prints an error message.
+
+            Console.WriteLine("Enter a number between 1-9 to find your bonus points");
+            a = int.Parse(Console.ReadLine());
+            switch (a)
+            {
+                case 1:
+                case 2:
+                case 3: Console.WriteLine("Bonus points =>{0}", a*10);
+                    break;
+                case 4:
+                case 5:
+                case 6:
+                    Console.WriteLine("Bonus points =>{0}", a * 100);
+                    break;
+
+                case 7:
+                case 8:
+                case 9:
+                    Console.WriteLine("Bonus points =>{0}", a * 1000);
+                    break;
+                default:
+                    Console.WriteLine("The number is outside the range");
+                    break;
+            }
+
             Console.ReadKey();
+
+
 
         }
     }
