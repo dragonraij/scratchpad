7629fb426014e0de07a6b30f3b6e91dc7e96750f
Raj Prasad <rajeshwarrprasad@yahoo.com>
2015-10-17 18:28:33 +1300

    Use loop to find user min and max

--- MSA/Fundementals of Programming/Ch06 Loops/Loops1/Loops1/Program.cs
+++ MSA/Fundementals of Programming/Ch06 Loops/Loops1/Loops1/Program.cs
@@ -11,12 +11,14 @@ namespace Loops1
         static void Main(string[] args)
         {
             //Declare variables
+            int a, largest = 0, smallest = 999999;
+            bool valid = true;
 
 
             //1. Write a program that prints on the console the numbers from 1 to N.
             // The number N should be read from the standard input.
 
-            int a = 0;
+
             Console.WriteLine("Count up to what?");
             a = int.Parse(Console.ReadLine());
 
@@ -36,14 +38,42 @@ namespace Loops1
             Console.WriteLine("Enter number =>");
             a = int.Parse(Console.ReadLine());
 
-            for (int i = 1; i < a; i++)
+            for (int i = 1; i <= a; i++)
             {
                 if (!(i%3==0 && i%7==0))
                 {
                     Console.Write(i+" ");
                 }
             }
-                       Console.ReadKey();
+
+
+
+            // 3. Write a program that reads from the console a series of integers and
+            // prints the smallest and largest of them.
+
+            Console.WriteLine("Type a series of numbers to find the largest and the smallest of them");
+
+            do
+            {
+                valid = int.TryParse(Console.ReadLine(), out a);
+                if (!valid)
+                {
+                    break;
+                }
+                if (a>largest)
+                {
+                    largest = a;
+
+                }
+
+                if (a<smallest)
+                {
+                    smallest = a;
+                }
+            } while (valid);
+
+            Console.WriteLine("Smallest Number is {0} Largest Number is {1}", smallest, largest);
+                                   Console.ReadKey();
         }
     }
 }
