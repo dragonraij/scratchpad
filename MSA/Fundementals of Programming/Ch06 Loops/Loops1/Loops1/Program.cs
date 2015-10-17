f189a8a10fe1d991f9ce2607ee4c36e1c3b480c6
Raj Prasad <rajeshwarrprasad@yahoo.com>
2015-10-17 15:25:54 +1300

    Using Loops
    Print number n times

---
+++ MSA/Fundementals of Programming/Ch06 Loops/Loops1/Loops1/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Loops1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Declare variables
+
+
+            //1. Write a program that prints on the console the numbers from 1 to N.
+            // The number N should be read from the standard input.
+
+            int a = 0;
+            Console.WriteLine("Count up to what?");
+            a = int.Parse(Console.ReadLine());
+
+            for (int i = 1; i <= a; i++)
+            {
+                Console.Write(i+ " ");
+            }
+            Console.WriteLine("");
+
+
+            Console.ReadKey();
+        }
+    }
+}
