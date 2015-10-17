4f055e48fe3af6c43f762aefdf40dd3585b408c9
Raj Prasad <rajeshwarrprasad@yahoo.com>
2015-10-17 15:33:28 +1300

    Selective prints using loops

--- MSA/Fundementals of Programming/Ch06 Loops/Loops1/Loops1/Program.cs
+++ MSA/Fundementals of Programming/Ch06 Loops/Loops1/Loops1/Program.cs
@@ -27,7 +27,23 @@ namespace Loops1
             Console.WriteLine("");
 
 
-            Console.ReadKey();
+
+            //2. Write a program that prints on the console the numbers from 1 to N,
+            //which are not divisible by 3 and 7 simultaneously.The number N
+            //should be read from the standard input.
+
+            Console.WriteLine("I print up to n excluding numbers divisble by 3 and 7 simultaneously");
+            Console.WriteLine("Enter number =>");
+            a = int.Parse(Console.ReadLine());
+
+            for (int i = 1; i < a; i++)
+            {
+                if (!(i%3==0 && i%7==0))
+                {
+                    Console.Write(i+" ");
+                }
+            }
+                       Console.ReadKey();
         }
     }
 }
