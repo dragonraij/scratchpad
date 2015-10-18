344240e485d683a82ff10adbd25d6891c99989f0
Raj Prasad <rajeshwarrprasad@yahoo.com>
2015-10-18 22:51:06 +1300

    Use loop to print deck of cards

--- MSA/Fundementals of Programming/Ch06 Loops/Loops1/Loops1/Program.cs
+++ MSA/Fundementals of Programming/Ch06 Loops/Loops1/Loops1/Program.cs
@@ -73,6 +73,72 @@ namespace Loops1
             } while (valid);
 
             Console.WriteLine("Smallest Number is {0} Largest Number is {1}", smallest, largest);
+
+
+
+            // 4.Write a program that prints all possible cards from a standard deck
+            // of cards, without jokers (there are 52 cards: 4 suits of 13 cards).
+
+            //define variables
+            string suit="", card="";
+            Console.WriteLine("The cards in a deck are:");
+            //loop for time for suits
+            for (int i = 0; i < 4; i++)
+            {
+                switch (i)
+                {
+                    case 0: suit = "hearts";
+                        break;
+                    case 1: suit = "clubs";
+                        break;
+                    case 2: suit = "diamonds";
+                        break;
+                    case 3: suit = "spades";
+                        break;
+                    default:
+                        suit = "Incorrect suit";
+                        break;
+                }
+
+                //loop 13 times for cards
+                for (int j = 0; j < 13; j++)
+                {
+                    switch (j)
+                    {
+                        case 0:
+                        case 1:
+                        case 2:
+                        case 3:
+                        case 4:
+                        case 5:
+                        case 6:
+                        case 7:
+                        case 8:
+                            card = (j + 2)+"";
+                            break;
+                        case 9:
+                            card = "Jack";
+                            break;
+                        case 10:
+                            card = "Queen";
+                            break;
+                        case 11:
+                            card = "King";
+                            break;
+                        case 12:
+                            card = "Ace";
+                            break;
+                        default:
+                            card = "Invalid card";
+                            break;
+                    }
+
+                    Console.WriteLine("{0, -7} of {1, -10}", card, suit);
+                }
+
+               
+            }
+            
                                    Console.ReadKey();
         }
     }
