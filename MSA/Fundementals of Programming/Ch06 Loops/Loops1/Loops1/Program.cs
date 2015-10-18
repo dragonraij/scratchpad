583a4d3321a0a84f8fa8167e684d68973a9a7fa5
Raj Prasad <rajeshwarrprasad@yahoo.com>
2015-10-18 22:55:41 +1300

    Print n fibonaci numbers

--- MSA/Fundementals of Programming/Ch06 Loops/Loops1/Loops1/Program.cs
+++ MSA/Fundementals of Programming/Ch06 Loops/Loops1/Loops1/Program.cs
@@ -138,6 +138,12 @@ namespace Loops1
 
                
             }
+
+
+
+            //5. Write a program that reads from the console number N and print the sum
+            //of the first N members of the Fibonacci sequence: 0, 1, 1, 2, 3, 5, 8,
+            //13, 21, 34, 55, 89, 144, 233, 377, …
             
                                    Console.ReadKey();
         }
