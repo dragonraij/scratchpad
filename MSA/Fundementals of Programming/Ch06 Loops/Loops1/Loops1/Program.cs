fea862d24deebbd100e53932e65c3f9d7f4362c8
Raj Prasad <rajeshwarrprasad@yahoo.com>
2015-10-19 16:04:44 +1300

    Divide two factorials

--- MSA/Fundementals of Programming/Ch06 Loops/Loops1/Loops1/Program.cs
+++ MSA/Fundementals of Programming/Ch06 Loops/Loops1/Loops1/Program.cs
@@ -164,7 +164,25 @@ namespace Loops1
             }
 
             //Write blank line before next problem
-                                   Console.ReadKey();
+            Console.Write("\n\n");
+
+
+
+            //6.Write a program that calculates N!/ K! for given N and K(1 < K < N).
+            Console.WriteLine("Enter N and K for: N!/ K! for given N and K(1 < K < N)");
+            int n = int.Parse(Console.ReadLine());
+            int k = int.Parse(Console.ReadLine());
+            temp = 1;
+
+            //Calculat the factorial
+            for (int i = k+1; i <= n; i++)
+            {
+                temp *= i;
+
+            }
+
+            Console.WriteLine("the factorial is {0}",temp);
+            Console.ReadKey();
         }
     }
 }
