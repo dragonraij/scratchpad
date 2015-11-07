9049ea2348b1b918539765af1361628ad660edfb
Raj Prasad <rajeshwarrprasad@yahoo.com>
2015-11-08 11:37:26 +1300

    Calculate min and max battles

---
+++ MSA/learnVisualStudio.net/cSharpFund/CS028_ChallengeForXmenBattleCount/ChallengeForXmenBattleCount/Default.aspx.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace ChallengeForXmenBattleCount
+{
+    public partial class Default : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            // Wolverine fewest battles
+            // Pheonix most battles
+
+            string[] names = new string[] { "Professor X", "Iceman", "Angel", "Beast", "Pheonix", "Cyclops", "Wolverine", "Nightcrawler", "Storm", "Colossus" };
+            int[] numbers = new int[] { 7, 9, 12, 15, 17, 13, 2, 6, 8, 13 };
+            
+            //declare variables to store details of max and min amounts and positions
+            int max = 0, min = 99, maxPosition = 0, minPosition = 0;
+
+            for (int i = 0; i < names.Length; i++)
+            {
+                if (numbers[i]>max)
+                {
+                    max = numbers[i];
+                    maxPosition = i;
+                }
+
+                if (numbers[i]<min )
+                {
+                    min = numbers[i];
+                    minPosition = i;
+                }
+            }
+
+            string result = string.Format("Minimum battles: {0}, fought by  {1} <br>Max battles: {2}, fought by {3}",
+                            min, names[minPosition], max, names[maxPosition]);
+
+
+            // Your Code Here!
+
+
+            resultLabel.Text = result;
+        }
+    }
+}
