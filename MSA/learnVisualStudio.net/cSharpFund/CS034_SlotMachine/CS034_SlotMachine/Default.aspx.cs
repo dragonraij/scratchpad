1f113dddb519a2ed9f22868a0db2e5ecf064da5e
Raj Prasad <rajeshwarrprasad@yahoo.com>
2015-11-10 21:00:25 +1300

    Create simple UI for slot machine

---
+++ MSA/learnVisualStudio.net/cSharpFund/CS034_SlotMachine/CS034_SlotMachine/Default.aspx.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace CS034_SlotMachine
+{
+    public partial class Default : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+            //spin wheel
+            //spin outcome
+            //calculate winnings
+            //update balance
+            //pull lever
+
+        }
+    }
+}
