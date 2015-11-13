fe53eadf1bab1dc68da1031d9122ccdae0251979
Raj Prasad <rajeshwarrprasad@yahoo.com>
2015-11-13 21:34:00 +1300

    Reverse a string

---
+++ MSA/learnVisualStudio.net/cSharpFund/CS035_PhunWithStrings/CS035_PhunWithStrings/Default.aspx.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace CS035_PhunWithStrings
+{
+    public partial class Default : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            String input = TextBox1.Text;
+            String output = "";
+            for (int i = input.Length-1; i>= 0; i--)
+            {
+                output= output + input[i].ToString();
+            }
+             Label1.Text = output;
+        }
+    }
+}
