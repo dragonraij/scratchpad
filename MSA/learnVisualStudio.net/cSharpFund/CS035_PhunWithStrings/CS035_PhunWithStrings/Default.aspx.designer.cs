902a3269d50e7e9099a2ccfad4b605e5f0e61483
Raj Prasad <rajeshwarrprasad@yahoo.com>
2015-11-15 15:39:30 +1300

    modify strings

--- MSA/learnVisualStudio.net/cSharpFund/CS035_PhunWithStrings/CS035_PhunWithStrings/Default.aspx.designer.cs
+++ MSA/learnVisualStudio.net/cSharpFund/CS035_PhunWithStrings/CS035_PhunWithStrings/Default.aspx.designer.cs
@@ -47,5 +47,32 @@ namespace CS035_PhunWithStrings {
         /// To modify move field declaration from designer file to code-behind file.
         /// </remarks>
         protected global::System.Web.UI.WebControls.Label Label1;
+        
+        /// <summary>
+        /// Label2 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label Label2;
+        
+        /// <summary>
+        /// Button2 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button Button2;
+        
+        /// <summary>
+        /// Label3 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label Label3;
     }
 }
