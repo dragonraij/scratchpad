7b618bbd96337904f7db4e0816fa86bb593673c5
Raj Prasad <rajeshwarrprasad@yahoo.com>
2015-11-21 11:49:17 +1300

    Add Student Model

--- MSA/Phase2/Day2/MSAAklFront/MSAAklFront/Models/Student.cs
+++ MSA/Phase2/Day2/MSAAklFront/MSAAklFront/Models/Student.cs
@@ -7,5 +7,12 @@ namespace MSAAklFront.Models
 {
     public class Student
     {
+        public int ID { get; set; }
+        public string LastName { get; set; }
+        public string FirstMidName { get; set; }
+        public DateTime EnrollmentDate { get; set; }
+
+      //  [JsonIgnore]
+        //public virtual ICollection<Enrollment> Enrollments { get; set; }
     }
 }
