e0d6f1613bbc1835c6a87b5e6a58abd9703ee4d1
Raj Prasad <rajeshwarrprasad@yahoo.com>
2015-11-23 13:03:42 +1300

    Enable chat using signalR

--- MSA/learnVisualStudio.net/MVC/SignalR/SignalR/MyHub1.cs
+++ MSA/learnVisualStudio.net/MVC/SignalR/SignalR/MyHub1.cs
@@ -1,16 +1,14 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Web;
 using Microsoft.AspNet.SignalR;
-
-namespace SignalR
+namespace SignalRChat
 {
-    public class MyHub1 : Hub
+    public class ChatHub : Hub
     {
-        public void Hello()
+        public void Send(string name, string message)
         {
-            Clients.All.hello();
+            // Call the broadcastMessage method to update clients.
+            Clients.All.broadcastMessage(name, message);
         }
     }
 }
