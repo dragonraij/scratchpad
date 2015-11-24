169a54c6edcb34f4761688dd815c45347775f3da
Raj Prasad <rajeshwarrprasad@yahoo.com>
2015-11-25 11:33:07 +1300

    Added Student, Enrollment and Course Class

---
+++ MSA/Student
@@ -0,0 +1,15 @@
+using System;
+using System.Collections.Generic;
+
+namespace ContosoUniversity.Models
+{
+    public class Student
+    {
+        public int ID { get; set; }
+        public string LastName { get; set; }
+        public string FirstName { get; set; }
+        public Text About { get; set; }
+        
+        public virtual ICollection<Enrollment> Enrollments { get; set; }
+    }
+}
---
+++ MSA/project/Enrollment.cs
@@ -0,0 +1,27 @@
+namespace ContosoUniversity.Models
+{
+    public enum Grade
+    {
+        A, B, C, D, F
+    }
+
+    public enum Status
+    {
+        Enrolled, Dropped, Completed
+    }
+
+    public class Enrollment
+    {
+        public int ID { get; set; }
+        public int CourseID { get; set; }
+        public int StudentID { get; set; }
+        public int PaperSemester {get; set;}
+
+        public Grade? Grade { get; set; }
+        public Status? Status {get; set;}
+
+        public virtual Course Course { get; set; }
+        public virtual Student Student { get; set; }
+        public virtual PaperSemester PaperSemester {get; set;}
+    }
+}
