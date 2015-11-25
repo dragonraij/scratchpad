e7c00a376387958b38f77998732f1535cdbadece
Raj Prasad <rajeshwarrprasad>
2015-11-25 15:46:26 +1300

    Update Student Class

--- MSA/project/ContosoUniversity/ContosoUniversity/Models/Student.cs
+++ MSA/project/ContosoUniversity/ContosoUniversity/Models/Student.cs
@@ -5,10 +5,12 @@ namespace ContosoUniversity.Models
 {
     public class Student
     {
-        public int ID { get; set; }
+        public int StudentID { get; set; }
         public string LastName { get; set; }
-        public string FirstMidName { get; set; }
-        public DateTime EnrollmentDate { get; set; }
+        public string FirstName { get; set; }
+        public string About { get; set; }
+
+
 
         public virtual ICollection<Enrollment> Enrollments { get; set; }
     }
