9778e898e0fbf00e14e8ecb64ccfdd1897cea944
Raj Prasad <rajeshwarrprasad>
2015-11-25 19:40:14 +1300

    Add Lecturer Class

---
+++ MSA/project/ContosoUniversity/ContosoUniversity/Models/Lecturer.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace ContosoUniversity.Models
+{
+
+    public class Lecturer
+    {
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int LecturerID { get; set; }
+        public string LastName { get; set; }
+        public string FirstName { get; set; }
+        public string Email { get; set; }
+        public short? Extension { get; set; }
+        public string Bio { get; set; }
+
+        //add link to profile pictures
+
+        public virtual ICollection<PaperSemester> PaperSemesters { get; set; }
+    }
+}
