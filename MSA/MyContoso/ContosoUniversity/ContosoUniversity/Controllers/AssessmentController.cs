b98698ffc60ee7fcd233c27308b120f26243bffa
Raj Prasad <rajeshwarrprasad>
2015-11-27 15:18:59 +1300

    Add additional Controllers from model

---
+++ MSA/MyContoso/ContosoUniversity/ContosoUniversity/Controllers/AssessmentController.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using ContosoUniversity.DAL;
+using ContosoUniversity.Models;
+
+namespace ContosoUniversity.Controllers
+{
+    public class AssessmentController : Controller
+    {
+        private SchoolContext db = new SchoolContext();
+
+        // GET: Assessment
+        public ActionResult Index()
+        {
+            var assessments = db.Assessments.Include(a => a.Enrollment);
+            return View(assessments.ToList());
+        }
+
+        // GET: Assessment/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Assessment assessment = db.Assessments.Find(id);
+            if (assessment == null)
+            {
+                return HttpNotFound();
+            }
+            return View(assessment);
+        }
+
+        // GET: Assessment/Create
+        public ActionResult Create()
+        {
+            ViewBag.EnrollmentID = new SelectList(db.Enrollments, "EnrollmentID", "EnrollmentID");
+            return View();
+        }
+
+        // POST: Assessment/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "AssessmentID,EnrollmentID,SubmissionTime,SubmissionLink,Marks")] Assessment assessment)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Assessments.Add(assessment);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.EnrollmentID = new SelectList(db.Enrollments, "EnrollmentID", "EnrollmentID", assessment.EnrollmentID);
+            return View(assessment);
+        }
+
+        // GET: Assessment/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Assessment assessment = db.Assessments.Find(id);
+            if (assessment == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.EnrollmentID = new SelectList(db.Enrollments, "EnrollmentID", "EnrollmentID", assessment.EnrollmentID);
+            return View(assessment);
+        }
+
+        // POST: Assessment/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "AssessmentID,EnrollmentID,SubmissionTime,SubmissionLink,Marks")] Assessment assessment)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(assessment).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.EnrollmentID = new SelectList(db.Enrollments, "EnrollmentID", "EnrollmentID", assessment.EnrollmentID);
+            return View(assessment);
+        }
+
+        // GET: Assessment/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Assessment assessment = db.Assessments.Find(id);
+            if (assessment == null)
+            {
+                return HttpNotFound();
+            }
+            return View(assessment);
+        }
+
+        // POST: Assessment/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Assessment assessment = db.Assessments.Find(id);
+            db.Assessments.Remove(assessment);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
