86ed5febcecf183fe9057842b1e8def9afd70aeb
Raj Prasad <rajeshwarrprasad@yahoo.com>
2015-12-02 17:32:55 +1300

    Add methods and properties to class and display sample data

--- MSA/learnVisualStudio.net/cSharpFund/CS036_Cars2/CS036_Cars2/Default.aspx.cs
+++ MSA/learnVisualStudio.net/cSharpFund/CS036_Cars2/CS036_Cars2/Default.aspx.cs
@@ -11,7 +11,34 @@ namespace CS036_Cars2
     {
         protected void Page_Load(object sender, EventArgs e)
         {
+            Car myNewCar = new Car();
+            myNewCar.Color = "Red";
+            myNewCar.Make = "Nissan";
+            myNewCar.Model = "Helios";
+            myNewCar.Year = 1999;
 
+            resultLabel.Text = String.Format("{0} {1} {2} {3} {4:C}", myNewCar.Year, myNewCar.Color, myNewCar.Make, myNewCar.Model, myNewCar.determineMarketValue());
         }
     }
+
+    class Car
+    {
+        public string Make { get; set; }
+        public string Model { get; set; }
+        public int Year { get; set; }
+        public string  Color { get; set; }
+
+        public double determineMarketValue() {
+            if (Year>2000)
+            {
+                return 10000;
+            }
+            else
+            {
+                return 5000;
+            }
+        }
+    }
+
+
 }
