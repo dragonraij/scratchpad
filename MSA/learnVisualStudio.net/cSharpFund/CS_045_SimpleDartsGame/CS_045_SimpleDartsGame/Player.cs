3126627f691fec5d33963fe30c46e3a6df4b1596
Raj Prasad <rajeshwarrprasad@yahoo.com>
2015-12-04 16:19:13 +1300

    Add Player Class

---
+++ MSA/learnVisualStudio.net/cSharpFund/CS_045_SimpleDartsGame/CS_045_SimpleDartsGame/Player.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace CS_045_SimpleDartsGame
+{
+    public class Player
+    {
+        public string Name { get; set; }
+        public int Score { get; set; }
+
+        public Player() {
+            this.Name = "Undefined";
+            this.Score = 0;
+        }
+
+        public Player (string name) : this()
+        {
+            this.Name = name;
+        }
+    }
+}
