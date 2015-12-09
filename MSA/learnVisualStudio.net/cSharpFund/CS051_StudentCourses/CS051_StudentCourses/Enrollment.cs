d8b5c4fe327deb8210c1f82172e43d8e5740809e
Raj Prasad <rajeshwarrprasad@yahoo.com>
2015-12-09 18:39:54 +1300

    Create enrollments class

---
+++ MSA/learnVisualStudio.net/cSharpFund/CS051_StudentCourses/CS051_StudentCourses/Enrollment.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace CS051_StudentCourses
+{
+    public class Enrollment
+    {
+        public char Grade { get; set; }
+        public Student Student { get; set; }
+        public Course Course { get; set; }
+    }
+}
