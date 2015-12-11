8f92af63884f1c8594c8f2e0c6f59a7e950f3cbf
Raj Prasad <rajeshwarrprasad@yahoo.com>
2015-12-11 20:05:29 +1300

    Create game class

--- MSA/learnVisualStudio.net/cSharpFund/CS051_WarGame/CS051_WarGame/Game.cs
+++ MSA/learnVisualStudio.net/cSharpFund/CS051_WarGame/CS051_WarGame/Game.cs
@@ -7,5 +7,21 @@ namespace CS051_WarGame
 {
     public class Game
     {
+        private Player player1 { get; set; }
+        private Player player2 { get; set; }
+
+
+        public Game() {
+            player1 = new Player(){ Name="Player 1"};
+            player2 = new Player() { Name = "Player 2" };
+        }
+
+
+        public Game(string name1, string name2)
+        {
+            player1 = new Player() { Name = name1 };
+            player2 = new Player() { Name = name2 };
+        }
+
     }
 }
