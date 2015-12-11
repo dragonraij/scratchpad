a9c2d3b9b671a90678e925426002fffe30061c58
Raj Prasad <rajeshwarrprasad@yahoo.com>
2015-12-11 19:53:50 +1300

    Add properties to player class and add method to play card

--- MSA/learnVisualStudio.net/cSharpFund/CS051_WarGame/CS051_WarGame/Player.cs
+++ MSA/learnVisualStudio.net/cSharpFund/CS051_WarGame/CS051_WarGame/Player.cs
@@ -7,5 +7,25 @@ namespace CS051_WarGame
 {
     public class Player
     {
+        public string Name { get; set; }
+        public List<Card> Hand = new List<Card>();
+
+        public Card PlayFirstCard() {
+            Card currentCard = Hand.ElementAt(0);
+            Hand.RemoveAt(0);
+            return currentCard;
+        
+        }
+
+
+
+        //object initializers
+        public Player() {
+            Name = "UnNamedPlayer";
+        }
+
+        public Player(string name) {
+            Name = name;
+        }
     }
 }
