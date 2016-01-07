04446fd7ddbccf075319ae94f80ceefef6e8b3fc
Raj Prasad <raj-prasad@live.com>
2016-01-08 11:11:31 +1300

    Add callback function

--- angular/tutorials/004Promises/app.js
+++ angular/tutorials/004Promises/app.js
@@ -2,13 +2,22 @@ function getData($timeout, $q){
 	return function(){
 		var defer = $q.defer()
 		
-		$timeout(function() {}, 2000)
+		//simulated async function
+		$timeout(function() {
+			defer.resolve('data recieved!')
+		}, 2000)
 		
 		return defer.promise	
 	}
 	
 }
 
+.run(function(getData){
+	var promise = getData()
+	.then(function(string){
+		console.log(string)
+	})
+})
 
 angular.module('app', [])
 .factory('getData', getData)
