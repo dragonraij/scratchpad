cf6c99b462388647c3b6fae92155def7f8655577
Raj Prasad <raj-prasad@live.com>
2016-01-07 16:41:34 +1300

    Update page using services

--- angular/tutorials/003Services/app.js
+++ angular/tutorials/003Services/app.js
@@ -19,7 +19,11 @@ angular.module('app').controller('ListCtrl', function(messages){
 
 angular.module('app').controller('PostCtrl', function(messages){
 	var self = this;
+	
+	self.newMessage = "Hello World!";
+	
 	self.addMessage = function(message){
 		messages.add(message);
+		self.newMessage = '';
 	};
 });
