fa847e799eac9050c6a8d0a15da39cd10da648c2
Raj Prasad <raj-prasad@live.com>
2016-01-08 16:52:43 +1300

    add addtab function to controller

--- angular/tutorials/007TabsDirective/app.js
+++ angular/tutorials/007TabsDirective/app.js
@@ -6,8 +6,9 @@
 			restrict: 'E',
 			transclude: true,
 			template: '<h2>Hello World!</h2> <div role="tabpanel" ng-transclude></div>',
+			require: '^tabset',
 			scope: {},
-			link: function(sscope, elem, attr){}
+			link: function(sscope, elem, attr, tabsetCtrl){}
 		}
 	})
 	.directive('tabset', function(){
@@ -21,6 +22,10 @@
 			controller: function(){
 				var self = this
 				self.tabs = []
+				
+				self.addTab = function addTab(tab){
+					self.tabs.push(tab)
+				}
 			}
 		}
 	})
