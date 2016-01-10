b7734294e1d08239db500994bf5fee8e71c33d13
Raj Prasad <raj-prasad@live.com>
2016-01-11 11:26:59 +1300

    Create dumbpass app

---
+++ angular/tutorials/007bTabsDirective/app.js
@@ -0,0 +1,39 @@
+;(function(window) {
+
+angular.module('app', [])
+
+// Define directives here
+	.directive('tab', function() {
+	return {
+		restrict: 'E',
+		transclude: true,
+		template: '<h2>Hello world!</h2> <div role="tabpanel" ng-transclude></div>',
+		require: '^tabset',
+    scope: { 
+      heading: '@'
+    },
+		link: function(scope, elem, attr, tabsetCtrl) {
+      tabsetCtrl.addTab(scope)
+    }
+	}
+	})
+	
+	.directive('tabset', function() {
+  return {
+    restrict: 'E',
+    transclude: true,
+    scope: { },
+    templateUrl: 'tabset.html',
+    bindToController: true,
+    controllerAs: 'tabset',
+    controller: function() {
+      var self = this
+      self.tabs = []
+      self.addTab = function addTab(tab) {
+        self.tabs.push(tab)
+      }
+    }
+  }
+})
+
+})(window);
