92f37ceef98d3e6da9d9254c5304cade25b727c0
Raj Prasad <rajeshwarrprasad>
2016-01-15 08:11:00 +1300

    Added component module for search

--- angular/weather dashboard/app/app.module.js
+++ angular/weather dashboard/app/app.module.js
@@ -1,6 +1,6 @@
 (function(){
     var name = "app",
-        requires = ["app.shell", "app.search", "app.weather", "app.forecast"];
+        requires = ["app.shell", "app.search", "app.weather", "app.forecast", "app.component"];
     
     angular.module(name, requires)
     .run(['$route', function($route){
