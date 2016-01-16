16fc8fed10a38dcd0e207cc1e3409a56c443b148
Raj Prasad <rajeshwarrprasad>
2016-01-17 00:29:49 +1300

    update forecast controller to get data from service

--- angular/weather dashboard/app/forecast/forecast.ctrl.js
+++ angular/weather dashboard/app/forecast/forecast.ctrl.js
@@ -1,6 +1,22 @@
 (function(){
     angular.module("app.forecast")
-    .controller("Forecast", function($scope){
+    .controller("Forecast", function($scope, $routeParams, weatherSvc){
+        $scope.forecast = null;
         
+        if ($routeParams.id !==undefined){
+            getForecast($routeParams.id);
+        }
+        
+        function getForecast(id){
+            weatherSvc.getForecast(id)
+                .then(
+                function(response){
+                    $scope.forecast = response;
+                    console.log($scope.forecast);
+                }, 
+            function(err){
+                console.log("could not get forecast", err);   
+            })
+        }
     });
 })();
