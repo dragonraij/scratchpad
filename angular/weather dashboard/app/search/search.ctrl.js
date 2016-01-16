83ee71156bf3182ec5b1302548bd19072d61be5d
Raj Prasad <rajeshwarrprasad>
2016-01-16 13:17:37 +1300

    link service to the serch button

--- angular/weather dashboard/app/search/search.ctrl.js
+++ angular/weather dashboard/app/search/search.ctrl.js
@@ -1,9 +1,19 @@
 (function(){
     angular.module("app.search")
-    .controller("Search", function($scope){
+    .controller("Search", function($scope, weatherSvc){
         $scope.$on("search", search);
         
         function search(evt, data){
+            weatherSvc.find(data.str)
+            .then(
+            function(response){
+                console.log(response);
+            },
+            function(err){
+                console.log("error finding cities", err);
+            }
+            )
+            //for debug
             console.log(data.str);
         }
         
